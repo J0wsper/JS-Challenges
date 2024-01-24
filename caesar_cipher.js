a163887f6aa454b26bc78a9ed641a4698bc1975e
Bram Schuijff <155334328+J0wsper@users.noreply.github.com>
2024-01-24 11:53:07 -0800

    Update caesar_cipher.js

--- caesar_cipher.js
+++ caesar_cipher.js
@@ -1,26 +1,27 @@
 class Alphabet {
     constructor() {
-        this.alphabet = {1: 'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h',9:'i',10:'j',11:'k',12:'l',13:'m',
-        14:'n',15:'o',16:'p',17:'q',18:'r',19:'s',20:'t',21:'u',22:'v',23:'w',24:'x',25:'y',26:'z'};
+        this.alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+        'n','o','p','q','r','s','t','u','v','w','x','y','z'];
     }
     length() {
         return this.alphabet.length;
     }
-    rotate(index, rot) {
-        for (let i = 0; i < this.alphabet.length; i++) {
-            this.alphabet[i] = index.alphabet[i+rot]; 
-        }
+    find(val) {
+        return this.alphabet.indexOf(val);
+    }
+    get(index) {
+        return this.alphabet[index];
     }
 }
 
 function caesarCipher(string, rot) {
     const alphabet = new Alphabet();
-    let cipher = new Alphabet();
-    cipher.rotate(alphabet,rot);
     const stringArray = Array.from(string);
+    const rotatedStringArray = [];
     for (e of stringArray) {
-        
+        const index = alphabet.find(e);
+        const returned = alphabet.get(index+rot);
+        rotatedStringArray.push(returned);
     }
+    return rotatedStringArray;
 }
-
-console.log(caesarCipher('gee',1))
